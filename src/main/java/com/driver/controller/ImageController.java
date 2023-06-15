564260213e2cc3c4f977d799cb3e5e356abeee40
naveen-kk4 <naveennig2001@gmail.com>
2023-06-16 01:47:18 +0530

    fisrt

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -19,7 +19,7 @@ public class ImageController {
 
     @GetMapping("/countImagesInScreen/{id}/{screenDimensions}")
     public ResponseEntity<Integer> countImagesInScreen(@PathVariable int id, @PathVariable String screenDimensions){
-        int count = 1;
+        int count = imageService.countImagesInScreen(id,screenDimensions);
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 

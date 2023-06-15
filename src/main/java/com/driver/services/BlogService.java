365c8d8f60dcba98d668faf0fcc1c5a69fdf0dfb
naveen-kk4 <naveennig2001@gmail.com>
2023-06-16 04:15:11 +0530

    first

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -17,19 +17,18 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public User createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) {
 
+        User user = userRepository1.findById(userId).get();
 
         Blog blog = new Blog();
         blog.setContent(content);
         blog.setTitle(title);
 
-        User user = userRepository1.findById(userId).get();
-
         blog.setUser(user);
         user.getBlogList().add(blog);
         userRepository1.save(user);
-        return user;
+        return blog;
 
     }
 
